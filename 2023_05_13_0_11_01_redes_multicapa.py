af4db50fa91625bcd2b405733153fa50190c03e8
Alnitakr51 <gadieljj34@gmail.com>
2023-05-14 02:20:04 +0200

    Redes Neuronales

---
+++ 2023_05_13_0_11_01_redes_multicapa.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 14 00:38:31 2023
+
+@author: Gadiel Jimenez
+"""
+
+import tensorflow as tf
+import numpy as np
+
+# Datos de entrenamiento
+X = np.array([[0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
+y = np.array([[0], [1], [1], [0]])
+
+# Parámetros de la red neuronal
+n_input = 3
+n_hidden1 = 4
+n_hidden2 = 4
+n_output = 1
+
+# Modelo de la red neuronal
+modelo = tf.keras.Sequential([
+    tf.keras.layers.Dense(n_hidden1, input_shape=(n_input,), activation='relu'),
+    tf.keras.layers.Dense(n_hidden2, activation='relu'),
+    tf.keras.layers.Dense(n_output, activation='sigmoid')
+])
+
+# Compilamos el modelo
+modelo.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Entrenamiento del modelo
+modelo.fit(X, y, epochs=1000, verbose=0)
